9fa289a560c2c22fb8696f4b4248f46826f88db8
harshmohansason <harshsason2000@gmail.com>
2025-05-24 09:05:33 +0530

    Fixed the CORS handler function, created a middleware which sends the request to the next handler

--- shared/handle_cors.go
+++ shared/handle_cors.go
@@ -1,29 +1,46 @@
 package shared
 
 import (
-	"net/http" 
-	"os")
+	"net/http"
+)
 
-func HandleCors(response http.ResponseWriter, request *http.Request) bool {
+func CORSHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		
+		corsEnabledFunction(response, request)
+		
+		next.ServeHTTP(response, request)
+	})
+}
+
+func corsEnabledFunction(response http.ResponseWriter, request *http.Request) {
 	
-	var origin string
-	if os.Getenv("ENV") == "DEV" {
-		origin = "http://localhost:3000"
-	} else {
-		origin = "https://ahschemicalsdebug.web.app"
+	//Pre defined allowed origins
+	allowedOrigins := map[string]bool{
+		"http://localhost:3000": true,
+		"https://ahschemicalsdebug.web.app": true,
 	}
 
-	// Cors ehaders
+	//Check if the origin exists
+	var origin string
+	if allowedOrigins[request.Header.Get("Origin")] {
+		origin = request.Header.Get("Origin")
+	}
+
+	// Set CORS headers for the preflight request
+	if request.Method == http.MethodOptions {
+		response.Header().Set("Access-Control-Allow-Origin", origin)
+		response.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		response.Header().Set("Access-Control-Allow-Credentials", "true")
+		response.Header().Set("Access-Control-Max-Age", "3600")
+		response.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	// Set CORS headers for the main request.
 	response.Header().Set("Access-Control-Allow-Origin", origin)
 	response.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	response.Header().Set("Access-Control-Allow-Credentials", "true")
-
-	// Pre flight request
-	if request.Method == http.MethodOptions {
-		response.WriteHeader(http.StatusOK)
-		return true
-	}
-
-	return false 
 }
