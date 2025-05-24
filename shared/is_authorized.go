950165be2a4029ef052b6a53eae4e33c2aaaf0a0
harshmohansason <harshsason2000@gmail.com>
2025-05-24 12:36:39 +0530

    Fixed authroziation error to check whether user is admin or not

--- shared/is_authorized.go
+++ shared/is_authorized.go
@@ -3,31 +3,30 @@ package shared
 import (
 	"errors"
 	"net/http"
-	"os"
+	"strings"
 )
 
-//Only using this in production.
+//Checks custom claims to detect if an admin or not
 func IsAuthorized(request *http.Request) error {
+
+		ctx := request.Context() 
 		
-		//True in local dev
-		if os.Getenv("ENV") == "DEV"{
-			return nil
+		//Process the authroization header
+		authHeader := request.Header.Get("Authorization")
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+    		return errors.New("Invalid auth header")
 		}
 
-		ctx := request.Context()
-			
-		//Check if the cookie named session exists. 
-		tokenCookie, err := request.Cookie("session")
-		if err != nil{
-			return err
-		}
-
-		idToken := tokenCookie.Value 
+		//Get the idtoken
+		idToken := parts[1]
 		token, err := AuthClient.VerifyIDToken(ctx, idToken)
 		if err != nil {
 			return err
 		}
-		if !token.Claims["isAdmin"].(bool){
+		
+		//Check if the user is admin or not
+		if !token.Claims["admin"].(bool){
 			return errors.New("Unauthroized. Only admins are allowed to perform this operation")
 		}
 
