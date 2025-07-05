f05d42d51e48c4c0da382e70a18702a03af45aa3
harshmohansason <harshsason2000@gmail.com>
2025-07-05 06:03:12 +0530

    Added a Get IP function to extract ip's from incoming requests

---
+++ shared/utils/get_ip.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"net"
+	"net/http"
+	"strings"
+)
+
+func GetIp(request *http.Request) string {
+	// Check X-Forwarded-For header
+	xff := request.Header.Get("X-Forwarded-For")
+	if xff != "" {
+		ips := strings.Split(xff, ",")
+		ip := strings.TrimSpace(ips[0])
+		if parsed := net.ParseIP(ip); parsed != nil {
+			return parsed.String()
+		}
+	}
+
+	// Check X-Real-IP header
+	xrip := request.Header.Get("X-Real-Ip")
+	if xrip != "" {
+		if parsed := net.ParseIP(xrip); parsed != nil {
+			return parsed.String()
+		}
+	}
+
+	// Fallback to RemoteAddr
+	ip, _, err := net.SplitHostPort(request.RemoteAddr)
+	if err == nil {
+		if parsed := net.ParseIP(ip); parsed != nil {
+			return parsed.String()
+		}
+	}
+
+	// If all else fails
+	return ""
+}
