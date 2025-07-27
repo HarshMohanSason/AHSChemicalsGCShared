7c1dbf520e2f6c49bbee053e0f92dc1939fa8001
harshmohansason <harshsason2000@gmail.com>
2025-07-27 15:35:55 +0530

    Forgot to add the staging url

--- shared/constants/cors.go
+++ shared/constants/cors.go
@@ -2,6 +2,6 @@ package constants
 
 const (
 	CorsAllowOriginDebug      = "http://localhost:3000"
-	CorsAllowOriginStaging    = ""
+	CorsAllowOriginStaging    = "https://ahschemicalsstaging.web.app"
 	CorsAllowOriginProduction = "https://azurehospitalitysupply.com"
 )
