a96a542cbc95f6ab6429f89c17c70f131ba4750d
harshmohansason <harshsason2000@gmail.com>
2025-08-02 04:28:57 +0530

    Removed and added files

--- shared/pdfgen/shipping_manifest/constants.go
+++
@@ -1,7 +0,0 @@
-package shipping_manifest
-
-//Shipping Manifest constant vars for pdf generation
-var (
-	ShippingManifestHeaders        = []string{"UNITS", "HM", "TYPE CONTAINER", "DESCRIPTION", "CLASS", "SKU", "NET WEIGHT", "GROSS WEIGHT NHM", "GROSS WEIGHT HM"}
-	ShippingManifestTableColWidths = []float64{13, 10, 20, 40, 15, 30, 20.6, 20.6, 20.6}
-)
