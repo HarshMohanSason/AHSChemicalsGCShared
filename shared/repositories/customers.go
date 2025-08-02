92e0b1cafa698ecd3686b83b030ef1267078e891
harshmohansason <harshsason2000@gmail.com>
2025-08-02 07:55:11 +0530

    Added update customer and create customer firestore functions

--- shared/repositories/customers.go
+++ shared/repositories/customers.go
@@ -34,7 +34,7 @@ func FetchCustomerFromFirestore(id string, ctx context.Context) (*models.Custome
 	return &customer, nil
 }
 
-//Only fetches active customers from firestore collection ('customers')
+// Only fetches active customers from firestore collection ('customers')
 func FetchAllCustomersFromFirestore(ctx context.Context) ([]models.Customer, error) {
 
 	customers, err := firebase_shared.FirestoreClient.Collection("customers").Where("isActive", "==", true).Documents(ctx).GetAll()
@@ -78,4 +78,40 @@ func SyncQuickbookCustomerRespToFirestore(qbItemsResponse *qbmodels.QBCustomersR
 	}
 	bulkWriter.Flush()
 	return nil
+}
+
+//UpdateCustomerInFirestore updates current customer document in firestore. 
+//
+// Params: 
+//  - ctx context of the async function
+//  - customerID is the id of the customer whose doc is being updated
+//  - details is of type any, the fields that need to be updated
+// 
+// Returns: 
+//  - error: if any 
+// Note: The details is of type any because any field can be updated when called. So its much
+// better to just pass the type any. In Addition to this, before calling this, make sure to always
+// and always double check if the key matches with the `firestore` key in the struct otherwise this
+// will create a new key with that value in document. 
+func UpdateCustomerInFirestore(ctx context.Context, customerID string, details any) error {
+	_, err := firebase_shared.FirestoreClient.Collection("customers").Doc(customerID).Set(ctx, details, firestore.MergeAll)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateCustomerInFirestore creates new customer document in firestore.
+//
+// Params:
+//   - ctx: context
+//   - customer: *models.Customer
+//
+// Returns:
+//   - error: error
+//
+func CreateCustomerInFirestore(ctx context.Context, customer *models.Customer) error {
+	_, err := firebase_shared.FirestoreClient.Collection("customers").Doc(customer.ID).Set(ctx, customer.ToMap())
+	return err
 }
