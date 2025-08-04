7ea20e0c9a8e41c1dbe06e2df6f292c74326af25
harshmohansason <harshsason2000@gmail.com>
2025-08-04 08:01:49 +0530

    Added the QBWebHookresponse struct with a method to unmarshal the incoming message from the subscription appropriately

--- shared/quickbooks/qbmodels/webhook.go
+++ shared/quickbooks/qbmodels/webhook.go
@@ -7,7 +7,7 @@ type QBWebHookResp struct {
 }
 
 type EventNotification struct {
-	RealmID         string         `json:"realmId"`
+	RealmID         string          `json:"realmId"`
 	DataChangeEvent DataChangeEvent `json:"dataChangeEvent"`
 }
 
@@ -16,12 +16,30 @@ type DataChangeEvent struct {
 }
 
 type Entity struct {
-	Name           string `json:"name"`         
-	ID             string `json:"id"`           
-	Operation      string `json:"operation"`   
-	LastUpdatedUTC string `json:"lastUpdated"`  
+	Name           string `json:"name"`
+	ID             string `json:"id"`
+	Operation      string `json:"operation"`
+	LastUpdatedUTC string `json:"lastUpdated"`
 }
 
-func (e *Entity) ToBytes() ([]byte, error){
+func (e *Entity) ToBytes() ([]byte, error) {
 	return json.Marshal(e)
+}
+
+// Represents the data payload pushed by quickbooks to the pubsub
+// The subscriber is made to only accept the data field. Gen2 functions 
+// still send this data field instead of sending the raw json bytes when 
+// pushed as a message to the topic so we need this struct to get the data 
+// then have a helper function to convert it to an Entity struct
+type QBWebHookPubMessage struct {
+	Data []byte `json:"data"`
+}
+
+func (qbwkm *QBWebHookPubMessage) ToEntity() (*Entity, error) {
+	var entity Entity
+	err := json.Unmarshal(qbwkm.Data, &entity)
+	if err != nil {
+		return nil, err
+	}
+	return &entity, nil
 }
