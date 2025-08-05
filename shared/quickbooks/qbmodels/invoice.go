236950e4b21580b153ba7d5affbf8a4a0a05258c
harshmohansason <harshsason2000@gmail.com>
2025-08-05 09:51:32 +0530

    Quickbooks invoice response api was written wrong, updated it

--- shared/quickbooks/qbmodels/invoice.go
+++ shared/quickbooks/qbmodels/invoice.go
@@ -9,40 +9,60 @@ import (
 )
 
 type Invoice struct {
-	ID          string               `json:"Id,omitempty"`
-	SyncToken   string               `json:"SyncToken,omitempty"`
-	CustomerRef Reference            `json:"CustomerRef"` // Required
-	TxnDate     string               `json:"TxnDate,omitempty"`
-	DueDate     string               `json:"DueDate,omitempty"`
-	TotalAmt    float64              `json:"TotalAmt,omitempty"`
-	Balance     float64              `json:"Balance,omitempty"`
-	CurrencyRef *Reference           `json:"CurrencyRef,omitempty"`
-	Line        []InvoiceLine        `json:"Line"`
-	CustomField []CustomField        `json:"CustomField,omitempty"`
-	MetaData    *quickbooks.MetaData `json:"MetaData,omitempty"`
+	ID                    string               `json:"Id,omitempty"`
+	SyncToken             string               `json:"SyncToken,omitempty"`
+	CustomerRef           Reference            `json:"CustomerRef"` // Required
+	TxnDate               string               `json:"TxnDate,omitempty"`
+	DueDate               string               `json:"DueDate,omitempty"`
+	DocNumber             string               `json:"DocNumber,omitempty"`
+	PrivateNote           string               `json:"PrivateNote,omitempty"`
+	Line                  []InvoiceLine        `json:"Line"` // Required
+	CustomField           []CustomField        `json:"CustomField,omitempty"`
+	TotalAmt              float64              `json:"TotalAmt,omitempty"`
+	Balance               float64              `json:"Balance,omitempty"`
+	ApplyTaxAfterDiscount bool                 `json:"ApplyTaxAfterDiscount,omitempty"`
+	PrintStatus           string               `json:"PrintStatus,omitempty"`
+	EmailStatus           string               `json:"EmailStatus,omitempty"`
+	CurrencyRef           *Reference           `json:"CurrencyRef,omitempty"`
+	ExchangeRate          float64              `json:"ExchangeRate,omitempty"`
+	MetaData              *quickbooks.MetaData `json:"MetaData,omitempty"`
+	BillAddr              *QBCustomerAddress   `json:"BillAddr,omitempty"`
+	ShipAddr              *QBCustomerAddress   `json:"ShipAddr,omitempty"`
+	SalesTermRef          *Reference           `json:"SalesTermRef,omitempty"`
+	DueDateBasedOn        string               `json:"DueDateBasedOn,omitempty"`
+	TrackingNum           string               `json:"TrackingNum,omitempty"`
+	ShipDate              string               `json:"ShipDate,omitempty"`
+	ShippingMethodRef     *Reference           `json:"ShippingMethodRef,omitempty"`
+	ClassRef              *Reference           `json:"ClassRef,omitempty"`
+	DepartmentRef         *Reference           `json:"DepartmentRef,omitempty"`
+	LocationRef           *Reference           `json:"LocationRef,omitempty"`
+	PO                    string               `json:"PO,omitempty"`
+	ARAccountRef          *Reference           `json:"ARAccountRef,omitempty"`
+	ProjectRef            *Reference           `json:"ProjectRef,omitempty"`
 }
 
 func NewInvoice(order *models.Order) *Invoice {
 
-	invoice :=  &Invoice{
+	invoice := &Invoice{
 		CustomerRef: Reference{Value: order.Customer.ID, Name: order.Customer.Name},
 		TxnDate:     time.Now().Format("2006-01-02"),
 		DueDate:     time.Now().AddDate(0, 0, 30).Format("2006-01-02"),
 		TotalAmt:    order.Total,
+		PO:          order.ID,
 	}
 	invoice.AddLines(order)
 	return invoice
 }
 
-func (i *Invoice) ToBytes() ([]byte, error){
+func (i *Invoice) ToBytes() ([]byte, error) {
 	return json.Marshal(i)
 }
 
 func (i *Invoice) AddLines(order *models.Order) {
 	invoiceLines := make([]InvoiceLine, 0)
-	for _ , item := range order.Items{
+	for _, item := range order.Items {
 		line := InvoiceLine{
-			DetailType: "SalesItemLineDetail",
+			DetailType:  "SalesItemLineDetail",
 			Description: item.GetFormattedDescription(),
 			Amount:      item.GetTotalPrice(),
 		}
@@ -66,13 +86,13 @@ type InvoiceLine struct {
 	GroupLineDetail     *GroupLineDetail     `json:"GroupLineDetail,omitempty"`
 }
 
-func (i *InvoiceLine) SetSalesItemLineDetail(item *models.Product){
+func (i *InvoiceLine) SetSalesItemLineDetail(item *models.Product) {
 	detail := &SalesItemLineDetail{
 		ItemRef: Reference{
 			Value: item.ID,
 			Name:  item.Name,
 		},
-		Qty: float64(item.Quantity),
+		Qty:       float64(item.Quantity),
 		UnitPrice: item.Price,
 	}
 	i.SalesItemLineDetail = detail
@@ -83,6 +103,26 @@ type SalesItemLineDetail struct {
 	Qty        float64    `json:"Qty,omitempty"`
 	UnitPrice  float64    `json:"UnitPrice,omitempty"`
 	TaxCodeRef *Reference `json:"TaxCodeRef,omitempty"`
+}
+
+type TxnTaxDetail struct {
+	TxnTaxCodeRef Reference `json:"TxnTaxCodeRef"`
+	TotalTax      float64   `json:"TotalTax"`
+	TaxLine       []TaxLine `json:"TaxLine,omitempty"`
+}
+
+type TaxLine struct {
+	Amount        float64       `json:"Amount"`
+	DetailType    string        `json:"DetailType"`
+	TaxLineDetail TaxLineDetail `json:"TaxLineDetail"`
+}
+
+type TaxLineDetail struct {
+	TaxRateRef       Reference `json:"TaxRateRef"`
+	NetAmountTaxable float64   `json:"NetAmountTaxable"`
+	PercentBased     bool      `json:"PercentBased"`
+	TaxPercent       float64   `json:"TaxPercent"`
+	TaxInclusiveAmt  float64   `json:"TaxInclusiveAmount,omitempty"`
 }
 
 type DiscountLineDetail struct {
@@ -103,11 +143,10 @@ type CustomField struct {
 }
 
 type QBInvoiceResponse struct {
-	QueryResponse struct {
-		Invoice []Invoice `json:"Invoice"`
-	} `json:"QueryResponse"`
+	Invoice Invoice `json:"Invoice"`
+	Time    string  `json:"time"`
 }
 
-func (qb *QBInvoiceResponse) GetInvoiceID() string{
-	return qb.QueryResponse.Invoice[0].ID
+func (qb *QBInvoiceResponse) GetInvoiceID() string {
+	return qb.Invoice.ID
 }
