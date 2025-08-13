fbcc5ff225a2ebd43844891bf34709622384d4f6
harshmohansason <harshsason2000@gmail.com>
2025-08-13 21:31:59 +0530

    Added FetchAssignedAdminsForCustomer to fetch the account details for admins. Helpful in sending emails to admins of each assigned property when there is a change in order

--- shared/repositories/user_account.go
+++ shared/repositories/user_account.go
@@ -3,23 +3,28 @@ package repositories
 import (
 	"context"
 
-	"cloud.google.com/go/firestore"
+	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/constants"
 	firebase_shared "github.com/HarshMohanSason/AHSChemicalsGCShared/shared/firebase"
 	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/models"
 )
 
-//Only adds brands and customers to firestore
-func CreateUserAccountInFirestore(userAccount *models.UserAccountUpdate, ctx context.Context) error {
-	_, err := firebase_shared.FirestoreClient.Collection("users").Doc(userAccount.UID).Set(ctx, userAccount.ToMap())
-	return err
-}
+func FetchAssignedAdminsForCustomer(ctx context.Context, customerID string) ([]*models.UserAccount, error) {
+	docSnapshots, err := firebase_shared.FirestoreClient.Collection("users").
+		Where("role", "==", constants.RoleAdmin).
+		Where("customers", "array-contains", customerID).
+		Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
 
-func UpdateUserAccountInFirestore(userAccount *models.UserAccountUpdate, ctx context.Context) error {
-	_, err := firebase_shared.FirestoreClient.Collection("users").Doc(userAccount.UID).Set(ctx, userAccount.ToMap(), firestore.MergeAll)
-	return err
-}
-
-func DeleteUserAccountInFirestore(id string, ctx context.Context) error {
-	_, err := firebase_shared.FirestoreClient.Collection("users").Doc(id).Delete(ctx)
-	return err
+	userAccounts := make([]*models.UserAccount, 0)
+	for _, docSnapshot := range docSnapshots {
+		var userAccount models.UserAccount
+		err = docSnapshot.DataTo(&userAccount)
+		if err != nil {
+			return nil, err
+		}
+		userAccounts = append(userAccounts, &userAccount)
+	}
+	return userAccounts, nil
 }
