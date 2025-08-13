9078fb052701565b5b39fe7082ef69d67ddcaf17
harshmohansason <harshsason2000@gmail.com>
2025-08-13 21:37:15 +0530

    added AddRecipientsFromAccounts func

--- shared/send_email/types.go
+++ shared/send_email/types.go
@@ -1,9 +1,13 @@
 package send_email
 
+import (
+	"github.com/HarshMohanSason/AHSChemicalsGCShared/shared/models"
+)
+
 type Attachment struct {
 	Base64Content string `json:"name"`
 	MimeType      string `json:"data"`
-	FileName      string `json:"content_type"` //FileName of the attachment. Must have an extension because on some mail applications, the attachment cannot be viewed if it does not have an extension. Eg: "purchase_order.pdf"
+	FileName      string `json:"content_type"` //FileName of the attachment. Must have an extension because on some mail applications like outlook mobile, the attachment cannot be viewed if it does not have an extension. Eg: "purchase_order.pdf"
 }
 
 type EmailMetaData struct {
@@ -13,9 +17,24 @@ type EmailMetaData struct {
 	Attachments []Attachment      `json:"attachments"` //List of attachments for the email
 }
 
+func (m *EmailMetaData) AddRecipient(email, name string) {
+	m.Recipients[email] = name
+}
+
+func (m *EmailMetaData) AddRecipientsFromAccounts(userAccounts []*models.UserAccount) {
+	for _, userRecord := range userAccounts {
+		m.AddRecipient(userRecord.Email, userRecord.Name)
+	}
+}
+
+func (m *EmailMetaData) AddData(key string, value any) {
+	m.Data[key] = value
+}
+
 func (m *EmailMetaData) AddAttachment(attachment Attachment) {
 	m.Attachments = append(m.Attachments, attachment)
 }
+
 func (m *EmailMetaData) AddAttachments(attachments []Attachment) {
 	m.Attachments = append(m.Attachments, attachments...)
 }
