9e34273278c9d067d0f776db146af3354d16a86d
harshmohansason <harshsason2000@gmail.com>
2025-08-26 18:44:48 +0530

    time.Now() is time.Now().UTC()

--- shared/repositories/tests/contact_us_test.go
+++ shared/repositories/tests/contact_us_test.go
@@ -12,14 +12,14 @@ import (
 func TestContactUsSaveToFirestore(t *testing.T) {
 	ip := "123.12.21.0"
 	ctx := context.Background()
-	
+
 	contactForm := &models.ContactUsForm{
-		Name: "test",
-		Email: "test",
-		Phone: "2312123123",
-		Location: "2040 N Preisker lane",
-		Message: "test",
-		Timestamp: time.Now().Add(time.Hour * 48),
+		Name:      "test",
+		Email:     "test",
+		Phone:     "2312123123",
+		Location:  "2040 N Preisker lane",
+		Message:   "test",
+		Timestamp: time.Now().UTC().Add(48 * time.Hour),
 	}
 
 	err := repositories.SaveContactUsToFirestore(contactForm, ip, ctx)
