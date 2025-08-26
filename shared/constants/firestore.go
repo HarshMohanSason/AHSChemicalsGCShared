a021705aeb60ffbad47468a2b13fc3f0bf9e0e57
harshmohansason <harshsason2000@gmail.com>
2025-08-26 14:46:30 +0530

    Created constants for all the firestore collection names

---
+++ shared/constants/firestore.go
@@ -0,0 +1,13 @@
+package constants
+
+// Firestore Collection Constants
+const (
+	OrdersCollection                = "orders"
+	ProductsCollection              = "products"
+	CustomersCollection             = "customers"
+	UsersCollection                 = "users"
+	ProductsPricesPerCustCollection = "products_prices_per_customer"
+	QuickBooksStateCollection       = "quickbooks_states"
+	QuickBooksTokenCollection       = "quickbooks_tokens"
+	ContactUsCollection             = "contact_us"
+)
