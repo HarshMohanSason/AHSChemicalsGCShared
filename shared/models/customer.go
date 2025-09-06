95efebf05a229b4d3da2a711e6fc34aa02b224b0
harshmohansason <harshsason2000@gmail.com>
2025-09-06 16:36:34 +0530

    added GetUpdatedCustomerDetails from services package to not have any import cycles

--- shared/models/customer.go
+++ shared/models/customer.go
@@ -45,3 +45,46 @@ func (c *Customer) ToMap() map[string]any {
 		"updatedAt": firestore.ServerTimestamp,
 	}
 }
+
+func GetUpdatedCustomerDetails(updated, oldCustomer *Customer) map[string]any {
+	if updated == nil || oldCustomer == nil {
+		return nil
+	}
+
+	changedValues := make(map[string]any)
+
+	if updated.IsActive != oldCustomer.IsActive {
+		changedValues["isActive"] = updated.IsActive
+	}
+	if updated.Name != oldCustomer.Name {
+		changedValues["name"] = updated.Name
+	}
+	if updated.Email != oldCustomer.Email {
+		changedValues["email"] = updated.Email
+	}
+	if updated.Phone != oldCustomer.Phone {
+		changedValues["phone"] = updated.Phone
+	}
+	if updated.Address1 != oldCustomer.Address1 {
+		changedValues["address1"] = updated.Address1
+	}
+	if updated.City != oldCustomer.City {
+		changedValues["city"] = updated.City
+	}
+	if updated.State != oldCustomer.State {
+		changedValues["state"] = updated.State
+	}
+	if updated.Zip != oldCustomer.Zip {
+		changedValues["zip"] = updated.Zip
+	}
+	if updated.Country != oldCustomer.Country {
+		changedValues["country"] = updated.Country
+	}
+
+	if len(changedValues) == 0 {
+		return nil
+	}
+	changedValues["updatedAt"] = firestore.ServerTimestamp
+
+	return changedValues
+}
